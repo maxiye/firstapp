9986065f3ab469a8c119ae816b1e8bd46e0becb8
zyl <912877398@qq.com>
2017-05-19 18:28:51 +0800

    dsa 啊撒旦法撒旦法 t sadfsdaf sadfsadf sadf
    t
    qit
    a

--- app/src/main/java/com/firstapp/com/first/MainActivity.java
+++ app/src/main/java/com/firstapp/com/first/MainActivity.java
@@ -1,13 +1,23 @@
 package com.firstapp.com.first;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "com.firstapp.com.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sendMsg(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText et = (EditText)findViewById(R.id.edit_message);
+        String msg = et.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,msg);
+        startActivity(intent);
     }
 }
