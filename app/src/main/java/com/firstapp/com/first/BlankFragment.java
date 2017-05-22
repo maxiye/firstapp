dbc98861ec4c2718c30a92b119daf3bb785e68be
zyl <912877398@qq.com>
2017-05-22 19:02:27 +0800

    applist

---
+++ app/src/main/java/com/firstapp/com/first/BlankFragment.java
@@ -0,0 +1,171 @@
+package com.firstapp.com.first;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link BlankFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link BlankFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BlankFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    public static final String ARG_1 = "param1";
+    public static final String ARG_2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment BlankFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static BlankFragment newInstance(String param1, String param2) {
+        BlankFragment fragment = new BlankFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_1, param1);
+        args.putString(ARG_2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_1);
+            mParam2 = getArguments().getString(ARG_2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        PackageManager pm = getActivity().getPackageManager();
+        if (mParam1 != null && !mParam1.isEmpty()){
+            TextView tv = (TextView)getView().findViewById(R.id.frgtxt);
+            try {
+                ApplicationInfo app_info = pm.getApplicationInfo(mParam1,1);
+                CharSequence appname = pm.getApplicationLabel(app_info);
+                tv.setText(appname);
+            }catch (PackageManager.NameNotFoundException e){
+                ArrayList<ApplicationInfo> al = (ArrayList)pm.getInstalledApplications(PackageManager.GET_META_DATA);
+                ListView lv = (ListView) getActivity().findViewById(R.id.l_v);
+                ArrayList<CharSequence> strs = new ArrayList<CharSequence>();
+                for (ApplicationInfo ai:al){
+                    if(pm.getApplicationLabel(ai).toString().toLowerCase().indexOf(mParam1.toLowerCase())>=0){
+                        strs.add(pm.getApplicationLabel(ai).toString()+"："+ai.packageName);
+                    }
+                }
+                if(!strs.isEmpty()){
+                    lv.setAdapter(new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1, (String[])strs.toArray(new String[strs.size()])));
+                }else{
+                    for (ApplicationInfo ai:al){
+                        if(ai.packageName.toLowerCase().indexOf(mParam1.toLowerCase())>=0){
+                            strs.add(pm.getApplicationLabel(ai).toString()+"："+ai.packageName);
+                        }
+                    }
+                    if(!strs.isEmpty()){
+                        lv.setAdapter(new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1, (String[])strs.toArray(new String[strs.size()])));
+                    }else{
+                        tv.setText("未找到");
+                    }
+
+                }
+
+            }
+        }else{
+            ArrayList<ApplicationInfo> al = (ArrayList)pm.getInstalledApplications(PackageManager.GET_META_DATA);
+            ViewGroup layout = (ViewGroup) getActivity().findViewById(R.id.frg_blk);
+            ListView lv = (ListView) getActivity().findViewById(R.id.l_v);
+            String[] strs = new String[al.size()];
+            int i = 0;
+            for (ApplicationInfo ai:al){
+                strs[i] = (String)pm.getApplicationLabel(ai)+"："+ai.packageName;
+                i++;
+            }
+            lv.setAdapter(new ArrayAdapter<String>(getActivity(),
+                    android.R.layout.simple_list_item_1, strs));
+        }
+
+    }
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        /*if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }*/
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
